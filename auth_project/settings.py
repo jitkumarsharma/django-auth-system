612640404bab7f8fe0c36cad95ac0b3be24ed164
Jit Kumar Sharma <jitnin2244@gmail.com>
2026-04-09 14:23:19 +0530

    Update settings.py

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -24,6 +24,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'auth_project.urls'
